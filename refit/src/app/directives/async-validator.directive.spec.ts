ef3ff5ec9b82b143d1b69e42a70e1472d205daa2
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-12-08 17:09:24 -0500

    fixed username checking

---
+++ refit/src/app/directives/async-validator.directive.spec.ts
@@ -0,0 +1,8 @@
+// import { AsyncValidatorDirective } from './async-validator.directive';
+
+// describe('AsyncValidatorDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new AsyncValidatorDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
