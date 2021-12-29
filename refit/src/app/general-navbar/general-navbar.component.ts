025735599a7b3e7116315500e372748043cce19d
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-12-29 00:17:10 -0500

    navbar still underway

--- refit/src/app/general-navbar/general-navbar.component.ts
+++ refit/src/app/general-navbar/general-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-general-navbar',
@@ -7,14 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ladyCats: string[]= ["Dresses", "Tops", "Accessories", "Lingerie", "Shoes", "Skirts", "Pants", "Jackets", "Others"]
-  menCats: string[] = ["Shorts", "Shirts", "Pants", "Underwear", "Shoes", "Accessories", "Others"]
+  menCats: string[] = ["Jackets", "Shirts", "Pants", "Underwear", "Shoes", "Accessories", "Others"]
   jewelry: string[] = ["Necklaces", "Pins", "Bracelets", "Earrings", "Rings", "Watches", "Others"]
   beauty: string[] = ["Make up", "Fragrance", "Skin care", "Hair", "Other"]
+  kids: string[] = ["Girls", "Boys"]
+  rentals: string[] = ["Ladies", "Men"]
 
   ngOnInit(): void {
+  }
+
+  goToLogin(){
+    this.route.navigate(["/login"])
+  }
+
+  goToSignup(){
+    this.route.navigate(["/phone"])
   }
 
 }
