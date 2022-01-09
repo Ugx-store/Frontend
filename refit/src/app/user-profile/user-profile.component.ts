92a3431658306b8d437071fdfb5f014311c87ab2
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2022-01-09 02:49:50 -0500

    profile page underway

--- refit/src/app/user-profile/user-profile.component.ts
+++ refit/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,7 +9,22 @@ export class UserProfileComponent implements OnInit {
 
   constructor() { }
 
+  buttonValue: number = 0;
+
   ngOnInit(): void {
+    this.buttonValue = 1
+  }
+
+  allButton(){
+    this.buttonValue = 1
+  }
+
+  sellingButton(){
+    this.buttonValue = 2
+  }
+
+  soldButton(){
+    this.buttonValue = 3
   }
 
 }
