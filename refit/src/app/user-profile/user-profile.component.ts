853d9307499def72e9419070dccf0a8ea7ea7497
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2022-05-06 01:53:48 -0400

    Added original price

--- refit/src/app/user-profile/user-profile.component.ts
+++ refit/src/app/user-profile/user-profile.component.ts
@@ -119,10 +119,19 @@ export class UserProfileComponent implements OnInit, OnDestroy{
   productBoost: BoostChecker = {}
   
   ngOnInit(): void {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
+    // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    //   return new bootstrap.Tooltip(tooltipTriggerEl)
+    // })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
     })
+
+    // var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+    //   container: 'body'
+    // })
 
     this.buttonValue = 1
     
